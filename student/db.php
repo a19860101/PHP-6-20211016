6505f71b40f51edb7823ce53ca84bccf4ca8666b
Zac <a19860101@gmail.com>
2021-10-30 12:31:12 +0800

    20211030

--- student/db.php
+++ student/db.php
@@ -6,19 +6,20 @@
 
 
     // 方法一
-    // // $db = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
-    // $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
+    // $db = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
+    $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
 
-    // mysqli_query($db,'SET NAMES utf8');
+    mysqli_query($db,'SET NAMES utf8');
+    echo '資料庫連線成功';
 
     //方法二
 
-    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
+    // $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
 
-    // print_r($db);
+    // // print_r($db);
 
-    if($db->connect_errno){
-        echo $db->connect_error;
-    }
+    // if($db->connect_errno){
+    //     echo $db->connect_error;
+    // }
 
-    $db->query('SET NAMES utf8');
+    // $db->query('SET NAMES utf8');
---
+++ student/index.php
@@ -0,0 +1,18 @@
+<?php
+    include('db.php');
+    // include_once('db.php');
+    // require('db.php');
+    // require_once('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>學員資料</h1>
+</body>
+</html>
