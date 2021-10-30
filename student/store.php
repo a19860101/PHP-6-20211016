ea7b2c03da6214cdf1e65a83bee512eec662e4b8
Zac <a19860101@gmail.com>
2021-10-30 15:07:59 +0800

    20211030

--- student/store.php
+++ student/store.php
@@ -8,16 +8,27 @@
     // echo "<br>";
     // echo $_POST["mail"];
 
+    // extract($_POST);
+    // echo $name;
+    // echo "<br>";
+    // echo $phone;
+    // echo "<br>";
+    // echo $mail;
+    // echo "<br>";
+    // echo $edu;
+    // echo "<br>";
+    // echo $gender;
+    // echo "<br>";
+    // // print_r($skill);
+    // echo implode(",",$skill);
+
+    //方法一
+    include('db.php');
     extract($_POST);
-    echo $name;
-    echo "<br>";
-    echo $phone;
-    echo "<br>";
-    echo $mail;
-    echo "<br>";
-    echo $edu;
-    echo "<br>";
-    echo $gender;
-    echo "<br>";
-    // print_r($skill);
-    echo implode(",",$skill);
+
+    $skill = implode(',',$skill);
+    $sql = "INSERT INTO students(name,mail,phone,edu,gender,skill)
+            VALUES('$name','$mail','$phone','$edu','$gender','$skill')";
+    mysqli_query($db,$sql);
+
+    header('location:index.php');
