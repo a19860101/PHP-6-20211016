42edabe49df7da65d4925c2d0274dd56788ad688
Zac <a19860101@gmail.com>
2021-10-30 14:39:00 +0800

    20211030

--- student/index.php
+++ student/index.php
@@ -20,9 +20,9 @@
     // }
 
     $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    foreach($data as $student){
-        echo $student['name'];
-    }
+    // foreach($data as $student){
+    //     echo $student['name'];
+    // }
     // print_r($data);
 ?>
 <!DOCTYPE html>
@@ -32,8 +32,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border: 1px solid #aaa;
+            padding: 10px;
+        }
+        table {
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <h1>學員資料</h1>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>電話</th>
+            <th>mail</th>
+            <th>性別</th>
+            <th>學歷</th>
+            <th>專長</th>
+        </tr>
+        <?php foreach($data as $student){ ?>
+            <tr>
+                <td><?php echo $student['id']; ?></td>
+                <td><?php echo $student['name']; ?></td>
+                <td><?php echo $student['phone']; ?></td>
+                <td><?php echo $student['mail']; ?></td>
+                <td><?php echo $student['gender']; ?></td>
+                <td><?php echo $student['edu']; ?></td>
+                <td><?php echo $student['skill']; ?></td>
+            </tr>
+        <?php } ?> 
+        <?php
+            // foreach($data as $student){
+            //     echo '<tr>';
+            //     echo '<td>'.$student['id'].'</td>';
+            //     echo '<td>'.$student['name'].'</td>';
+            //     echo '<td>'.$student['phone'].'</td>';
+            //     echo '<td>'.$student['mail'].'</td>';
+            //     echo '<td>'.$student['gender'].'</td>';
+            //     echo '<td>'.$student['edu'].'</td>';
+            //     echo '<td>'.$student['skill'].'</td>';
+            //     echo '</tr>';
+            // }
+        ?>
+    </table>
 </body>
 </html>
