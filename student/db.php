95d25f0b6eb88cc439902c3165231b6900203160
Zac <a19860101@gmail.com>
2021-10-30 12:18:08 +0800

    20211030

--- student/db.php
+++ student/db.php
@@ -4,4 +4,21 @@
     $db_pw = 'admin';
     $db_name = 'php-6-20211016';
 
-    $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+
+    // 方法一
+    // // $db = @mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
+    // $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫錯誤!');
+
+    // mysqli_query($db,'SET NAMES utf8');
+
+    //方法二
+
+    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
+
+    // print_r($db);
+
+    if($db->connect_errno){
+        echo $db->connect_error;
+    }
+
+    $db->query('SET NAMES utf8');
