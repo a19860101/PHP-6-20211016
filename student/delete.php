e540e90f3462bc7f2b75a918b2be5eb4d59ed3d5
Zac <a19860101@gmail.com>
2021-10-30 16:15:17 +0800

    20211030

--- student/delete.php
+++ student/delete.php
@@ -1,10 +1,17 @@
 <?php
-    //方法一
     include('db.php');
     extract($_POST);
+    
+    //方法一
+    // $sql = 'DELETE FROM students WHERE id = '.$id;
+    // mysqli_query($db,$sql);
 
-    $sql = 'DELETE FROM students WHERE id = '.$id;
-    mysqli_query($db,$sql);
+    //方法二
+    $sql = 'DELETE FROM students WHERE id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('i',$id);
+    $stmt->execute();
+
 
     echo '<script>alert("資料已刪除!");</script>';
     header('refresh:0;url=index.php');
