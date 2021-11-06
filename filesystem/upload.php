92199e8e6020db307e34919e8c7cc71904fef70e
Zac <a19860101@gmail.com>
2021-11-06 12:17:24 +0800

    20211106

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -4,6 +4,18 @@
     // print_r($_FILES['img']);
     extract($_FILES['img']);
 
+    
+    if(!is_dir('images')){
+        mkdir('images');
+    }
+    
+    $target = 'images/'.$name;
+
+    
     if($error == 0){
-        move_uploaded_file($tmp_name,$name);
+        if(move_uploaded_file($tmp_name,$target)){
+            echo '上傳成功';
+        }else{
+            echo '上船錯誤';
+        }
     }
