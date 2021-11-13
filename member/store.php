3388b399a44d6df023373b7f31de2db652e9d195
Zac <a19860101@gmail.com>
2021-11-13 11:03:47 +0800

    20211113

--- member/store.php
+++ member/store.php
@@ -5,6 +5,18 @@
 
     $pw = md5(sha1($pw));
 
+    $sql_check = 'SELECT * FROM users WHERE user = ?';
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->execute([$user]);
+
+    // echo $stmt_check->rowCount();
+    if($stmt_check->rowCount() > 0){
+        echo '<script>alert("此帳號已被使用，請重新輸入!");</script>';
+        header('refresh:0;url=create.php');
+        return;
+    }
+
+
     $sql = 'INSERT INTO users(user,pw,created_at)VALUES(?,?,?)';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$user,$pw,$now]);
