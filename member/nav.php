4c5b83ed901d97b1fc4ac19b7bab9e22e4f8aba0
Zac <a19860101@gmail.com>
2021-11-13 12:00:57 +0800

    20211113

--- member/nav.php
+++ member/nav.php
@@ -3,7 +3,7 @@
     <?php if(isset($_SESSION['AUTH'])){ ?>
         <span><?php echo $_SESSION['AUTH']['user']; ?>您好!</span>
         <a href="<?php echo $webroot.'logout.php'?>">登出</a>
-
+        <a href="<?php echo $webroot.'work.php'?>">工作頁面</a>
     <?php } ?>
     
     <?php if(!isset($_SESSION['AUTH'])){?>
--- member/work.php
+++ member/work.php
@@ -1,14 +1,11 @@
 <?php
-    include('../../pdo.php');
     session_start();
     $webroot = 'http://localhost/php-6-20211016/member/';
 
-    if($_SESSION['AUTH']['role'] != 0){
+    if(!$_SESSION['AUTH']){
         header('Location:'.$webroot);
         return;
     }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +16,6 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include('../nav.php'); ?>
-    <h1>管理介面</h1>
+    <h1>工作頁面</h1>
 </body>
 </html>
