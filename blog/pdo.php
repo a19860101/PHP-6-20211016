6e3fbd35b5909e748382c8203980084b21ee07b7
Zac <a19860101@gmail.com>
2021-11-13 16:05:37 +0800

    20211113

---
+++ blog/pdo.php
@@ -0,0 +1,33 @@
+<?php
+
+class DB {
+    function __construct(){
+        
+    }
+    function connect(){
+        $db_host = 'localhost';
+        $db_user = 'admin';
+        $db_pw = 'admin';
+        $db_name = 'php-6-20211016';
+        $db_charset = 'utf8mb4';
+        
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        try {
+        
+            $pdo = new PDO($dsn,$db_user,$db_pw);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            //主動例外
+        
+        }catch(PDOException $e){
+            // print_r($e);
+            echo $e->getMessage();
+        }
+        return $pdo;
+    }
+}
+
+
+
+date_default_timezone_set('Asia/Taipei');
+$now = date('Y-m-d H:i:s');
+
