39a0aa39cbfeb04075e9c50f67d276ad400c317b
Zac <a19860101@gmail.com>
2021-11-13 14:54:44 +0800

    20211113

--- class/basic-2.php
+++ class/basic-2.php
@@ -4,17 +4,17 @@
         public $name;
         public $skill;
         private $damage = 0;
-        protected $magic = 0;
+        protected $magic = 10;
 
         //方法
         function attack(){
-            return $this->damage = 3;
+            return $this->damage;
         }
         function walk(){
             return $this->name.'walk';
         }
         function magicAttack(){
-            return $this->magic = 50;
+            return $this->magic;
         }
     }
     class NPC extends Role {
@@ -22,17 +22,23 @@
         function talk(){
             return 'I am NPC';
         }
-        function NPCattack(){
-            return $this->damage = 15;
+        function npcAttack(){
+            return $this->damage;
+        }
+        function npcMagicAttack(){
+            return $this->magic;
         }
 
     }
 
     $user1 = new Role;
     // echo $user1->damage;
+    // echo $user1->magic;
     // echo $user1->attack();
     // echo $user1->magicAttack();
-
     $com1 = new NPC;
-
-    print_r($com1->magic);
+    // echo $com1->damage;
+    // echo $com1->magic;
+    // echo $com1->npcAttack();
+    // echo $com1->npcMagicAttack();
+    
