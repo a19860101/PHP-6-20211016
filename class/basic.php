0c53c0d29cc6f862972813b50a64195b80ba60ce
Zac <a19860101@gmail.com>
2021-11-13 14:04:04 +0800

    20211113

--- class/basic.php
+++ class/basic.php
@@ -5,6 +5,11 @@
         public $category;
         public $skill = [];
 
+        //方法
+        function attack(){
+            echo 'attack';
+        }
+
     }
 
     $user_1 = new Role;
@@ -12,4 +17,6 @@
     $user_1 -> category = '刺客';
     $user_1 -> skill = ['無暇追擊','落擲苦無','暮光朦影','離群手裡劍','刺客印記'];
 
-    print_r($user_1);
+    // print_r($user_1);
+
+    $user_1->attack();
