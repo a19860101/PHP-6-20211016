87a65a7767e8e7ecb52855b9ac64298919cd8696
Zac <a19860101@gmail.com>
2021-12-04 11:42:40 +0800

    20211204

--- blog/posts/create.php
+++ blog/posts/create.php
@@ -58,7 +58,32 @@
             'alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist '+
             'forecolor backcolor removeformat | insertfile image media template link anchor codesample ',
         toolbar_mode: 'sliding',
+        // 檔案上傳
+        image_title: true,
+        automatic_uploads: true,
+        file_picker_types: 'image',
+        file_picker_callback: function (cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
 
+            input.onchange = function () {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                eader.onload = function () {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+                reader.readAsDataURL(file);
+            };
+
+            input.click();
+        }
     });
 </script>
 
